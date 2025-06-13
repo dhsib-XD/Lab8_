ea016b3c53c4403506c7fb99de068c9b4eafd4bc
Nathan Alegria <nathanalegria02@gmail.com>
2025-06-13 08:47:35 -0600

    prueba

---
+++ Lab8/src/lab8/UsuarioJugador.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab8;
+
+/**
+ *
+ * @author Nathan
+ */
+public class UsuarioJugador {
+    private String usuario;
+    private String contra;
+    private int puntos;
+    
+    public Jugador(String usuario, String contra){
+        
+    }
+    
+}
